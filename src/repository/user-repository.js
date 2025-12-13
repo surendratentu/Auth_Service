7c9b586002234c13d33109355e5dd441295e7dbc
surendratentu <surendratentu2005@gmail.com>
2025-12-13 11:41:36 +0530

    Signup logic with password encryption done

---
+++ src/repository/user-repository.js
@@ -0,0 +1,30 @@
+const { User } = require('../models/index');
+
+class UserRepository {
+
+    async create(data) {
+        try {
+            const user = await User.create(data);
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
+
+    async destory(userId) {
+        try {
+            await User.destroy({
+                where: {
+                    id: userId
+                },
+            });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw error;
+        }
+    }
+}
+
+module.exports = UserRepository;
